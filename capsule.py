66f5875632563c8e861606d4a480cd722b58297f
Kyryl Zuiev <102185030+LordWhiskas@users.noreply.github.com>
2023-12-07 14:26:23 +0100

    capsule.py fixing

--- capsule.py
+++ capsule.py
@@ -1,3 +1,4 @@
+import os
 import time
 import torch
 import wandb
@@ -6,11 +7,9 @@ from torch import nn
 import torch.nn.functional as F
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
-<<<<<<< HEAD
 import cProfile
-=======
->>>>>>> 6d85ce9655f2d267e71b3ef6ab8e4794bece8ecc
-
+from dotenv import load_dotenv
+from pathlib import Path
 
 class MySampler(torch.utils.data.Sampler):  # Custom sampler for balancing classes in each batch
     def __init__(self, data_source):
@@ -119,10 +118,7 @@ class RoutingLayer(nn.Module):
         self.listC = []
 
     def forward(self, u):  # u = [batch, num_caps_in, caps_dim_in]
-<<<<<<< HEAD
         self.listC = []
-=======
->>>>>>> 6d85ce9655f2d267e71b3ef6ab8e4794bece8ecc
         u_hat = torch.einsum('ijnm, bin->bijm', self.weight,
                              u)  # u_hat = [batch, num_caps_in, num_caps_out, caps_dim_out]
         b = u.new_zeros(u.shape[0], self.caps_in, self.caps_out)  # b = [batch, num_caps_in, num_caps_out]
@@ -192,7 +188,6 @@ class CapsuleModel(nn.Module):
 
     def getC(self, x):
         caps = self.conv1(x)  # Feature extraction using convolutional blocks
-<<<<<<< HEAD
 
         caps = self.primaryCaps(
             caps)  # Primary caps layer, which changes shape of data from [channel, height, width] to [num_caps,
@@ -201,15 +196,7 @@ class CapsuleModel(nn.Module):
         caps = self.dropout(caps)
         _ = self.routing_final(caps)  # final routing layer, where we get the capsule for each class
         result = self.routing_final.listC
-=======
-        caps = self.primaryCaps(caps)  # Primary caps layer
-        caps = self.routing(caps)
-        caps = self.dropout(caps)
-        self.routing_final(caps)
-        result = self.routing_final.listC
-        self.routing_final.listC = []
->>>>>>> 6d85ce9655f2d267e71b3ef6ab8e4794bece8ecc
-        return result
+        return caps
 
 
 class Recon(nn.Module):
@@ -268,11 +255,7 @@ def train(lr=0.0035, coef=0.7, conv_size=None, capdim=None):
     if conv_size is None:
         conv_size = [8, 16]
     if capdim is None:
-<<<<<<< HEAD
         capdim = [(72, 4), (20, 15), (10, 20)]  # capdim = [(72, 4), (20, 15), (10, 20)]
-=======
-        capdim = [(72, 4), (10, 20)]  # capdim = [(72, 4), (20, 15), (10, 20)]
->>>>>>> 6d85ce9655f2d267e71b3ef6ab8e4794bece8ecc
     loaders = getMNIST()  # loader for dataset
     in_channels = 1  # number of channels of input images
     # recon = Recon(capdim=capdim[-1], out_channels=in_channels).to(device)  # simple recon
@@ -340,17 +323,13 @@ def train(lr=0.0035, coef=0.7, conv_size=None, capdim=None):
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    wandb.login(key="4ec84e680770fa5ef52e55bac394efac593c7552")
+    dotenv_path = Path('.env')
+    load_dotenv(dotenv_path=dotenv_path)
+    WANDB_API = os.getenv('API_KEY')
+    wandb.login(key=WANDB_API)
     device = "cuda:0"
     try:
         cProfile.run("train()", sort='cumulative')
     except Exception as e:
         print(f"An error occurred: {e}")
     wandb.finish()
-=======
-    wandb.login(key="YOUR_WANDB_KEY")
-    device = "cuda:0"
-    train()
-    wandb.finish()
->>>>>>> 6d85ce9655f2d267e71b3ef6ab8e4794bece8ecc
